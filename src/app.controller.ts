dea188ec6f4974d80195d7bbe42a5284dfd438ad
MjImani <mjimani.ir@gmail.com>
2022-01-30 23:58:17 +0330

    add prisma and postgres

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Get, Param, Query } from "@nestjs/common";
-import { AppService } from "./app.service";
+import { Controller, Get, Param, Post, Query, Body } from '@nestjs/common';
+import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {
+  constructor(
+    private readonly appService: AppService,
+    private readonly prismaService: PrismaService,
+  ) {
   }
 
   @Get()
@@ -11,8 +15,18 @@ export class AppController {
     return this.appService.getHello();
   }
 
-  @Get("test")
-  getHelloTest(@Param("key") key: string): string {
+  @Get('test')
+  getHelloTest(@Param('key') key: string): string {
     return this.appService.getHelloTest(key);
+  }
+
+  @Get('users')
+  async users() {
+    return await this.prismaService.user.findMany();
+  }
+
+  @Post('users')
+  async addUser(@Body() createUserDto: any) {
+    return await this.prismaService.user.create({ data: createUserDto });
   }
 }
